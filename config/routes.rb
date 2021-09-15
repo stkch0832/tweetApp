047aca3f6d53b8d19afc9c4f71486d17275359d6
stkch0832 <stkch0832@gmail.com>
2021-09-15 22:19:15 +0900

    users サインアップ、編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  post 'users/:id/update' => 'users#update'
+  get 'users/:id/edit' => 'users#edit'
+  post 'users/create' => 'users#create'
+  get 'signup' => 'users#new'
+  get 'users/index' => 'users#index'
+  get 'users/:id' => 'users#show'
+
   get 'posts/index' => 'posts#index'
   get 'posts/new' => 'posts#new'
   get 'posts/:id'=> 'posts#show'
@@ -8,7 +15,7 @@ Rails.application.routes.draw do
   post 'posts/:id/update' => 'posts#update'
   post 'posts/:id/destroy' => 'posts#destroy'
   
-  get '/' => "home#top"
-  get 'about' => "home#about"
+  get '/' => 'home#top'
+  get 'about' => 'home#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
