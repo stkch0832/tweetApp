0fc91450e3581038d8ad929d68e257d44bace2c5
stkch0832 <stkch0832@gmail.com>
2021-09-23 17:32:54 +0900

    Like table 新規作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_21_141526) do
+ActiveRecord::Schema.define(version: 2021_09_23_083148) do
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
