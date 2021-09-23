8ba9e1d02e83658180f8b7cb4294d80aafd85c6b
stkch0832 <stkch0832@gmail.com>
2021-09-23 19:36:32 +0900

    完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_23_083148) do
+ActiveRecord::Schema.define(version: 2021_09_23_095832) do
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2021_09_23_083148) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
